bee5f822b9e97d2cd9a92d8b7360e70836cad3bb
Ben Hernandez <benjammin234@gmail.com>
2021-01-29 15:02:12 -0800

    Simplified features

--- newtab.js
+++ newtab.js
@@ -31,16 +31,17 @@ function getDateAndTime() {
     months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
 
   let time = [
-    now.getHours(),
+    // now.getHours(),
+    ("0" + now.getHours()).slice(-2),
     ("0" + now.getMinutes()).slice(-2),
     ("0" + now.getSeconds()).slice(-2),
   ].join(":");
 
-  let am_pm = now.getHours() >= 12 ? "PM" : "AM";
+  // let am_pm = now.getHours() >= 12 ? "PM" : "AM";
 
   if (document.getElementById("date") !== null) {
     document.getElementById("date").innerHTML = date;
-    document.getElementById("time").innerHTML = time + " " + am_pm;
+    document.getElementById("time").innerHTML = time;
   }
 
   setInterval(getDateAndTime, 1000);
@@ -53,7 +54,7 @@ function displayWeather(responseJson) {
   console.log(responseJson);
 
   const temp = Math.round(((responseJson.main.temp - 273.15) * 9) / 5 + 32);
-  const current_conditions = responseJson.weather[0].main;
+  const current_conditions = responseJson.weather[0].description;
   const wind = Math.round(responseJson.wind.speed);
   const icon = responseJson.weather[0].icon;
 
@@ -103,3 +104,38 @@ async function getWeather() {
 }
 
 getWeather();
+
+// WHERE
+
+// function getTomTom(position) {
+//   const lat = position.coords.latitude;
+//   const long = position.coords.longitude;
+//   const userRiver = $('input[name="riverName"]:checked').val();
+//   const userRiverName = riverDescrip.find(userRiverName => userRiverName.id === userRiver);
+//   const userRiverCoords = userRiverName.takeout;
+//   const getTravelTimeURL = `https://api.tomtom.com/routing/1/calculateRoute/${lat}%2C${long}%3A${userRiverCoords}/json?avoid=unpavedRoads&key=MmbpnXLGCMLejulVsu5VFZOlWUSUivGs`
+//   fetch(getTravelTimeURL)
+//   .then(response => {
+//       if(response.ok) {
+//           return response.json();
+//       }
+//       throw new Error(response.statusText);
+//   })
+//   .then(responseJson => displayTomTom(responseJson))
+//   .catch(err => {
+//       $('#js-directions-err-msg').text(`TomTom threw a temper tantrum. Please try again in a few moments.`);
+//   });
+// }
+
+// function displayTomTom(responseJson) {
+//   const travelTime = responseJson.routes[0].summary.travelTimeInSeconds;
+//   const travelHours = Math.floor(travelTime / 3600);
+//   const travelMins = Math.floor((travelTime - (travelHours * 3600)) / 60);
+//   $('#js-travel-time').html(`<span class="travel">Time to get to take-out</span><br>${travelHours} hr and ${travelMins} min`);
+//   const travelDistance = responseJson.routes[0].summary.lengthInMeters;
+//   const distanceMiles = Math.round(travelDistance / 1609);
+//   $('#js-travel-distance').html(`<span class="travel">Distance to take-out</span><br>${distanceMiles} miles`)
+//   const rawETA = responseJson.routes[0].summary.arrivalTime;
+//   const ETA = rawETA.slice(11, 16);
+//   $('#js-eta').html(`<span class="travel">Take-out ETA</span><br>${ETA}`)
+// }
